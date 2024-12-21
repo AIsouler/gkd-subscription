652e516d52515715054c43ec64a1cfea14139d8f
AIsouler <zy43220@gmail.com>
2024-12-21 20:20:40 +0800

    feat(小米商城): 开屏广告

--- src/apps/com.xiaomi.shop.ts
+++ src/apps/com.xiaomi.shop.ts
@@ -5,6 +5,21 @@ export default defineGkdApp({
   name: '小米商城',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      priorityTime: 10000,
+      rules: [
+        {
+          fastQuery: true,
+          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/18218380',
+        },
+      ],
+    },
+    {
       key: 1,
       name: '全屏广告-弹窗广告',
       desc: '点击关闭',
