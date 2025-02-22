30d82fec44b375ca07f0403dc829af2b2992b9b8
AIsouler <zy43220@gmail.com>
2025-02-22 22:03:07 +0800

    perf: 开屏广告 (字节SDK)

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -39,9 +39,11 @@ export default defineGkdGlobalGroups([
         ],
       },
       {
-        key: 2,
-        matches:
-          'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true][text=null][visibleToUser=true] + TextView[visibleToUser=true][text=null][index=parent.childCount.minus(1)]', // 字节SDK
+        key: 2, // 字节SDK
+        anyMatches: [
+          '@View[clickable=true][text=null][visibleToUser=true] + TextView[index=parent.childCount.minus(1)][text=null] <n FrameLayout[childCount>2] >(7,8,9,10) [text*="第三方应用" || text*="扭动手机" || text*="点击或上滑"][visibleToUser=true]',
+          'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true][text=null][visibleToUser=true] + TextView[index=parent.childCount.minus(1)][text=null][visibleToUser=true]',
+        ],
       },
     ],
     apps: [...appList.openAdBlackListAppIDs]
