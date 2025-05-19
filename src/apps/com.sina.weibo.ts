9578760b6b5e2c38917096dba993ec6b9b8b2499
AIsouler <zy43220@gmail.com>
2025-05-19 13:18:31 +0800

    feat: rules

--- src/apps/com.sina.weibo.ts
+++ src/apps/com.sina.weibo.ts
@@ -7,16 +7,25 @@ export default defineGkdApp({
     {
       key: 1,
       name: '局部广告-悬浮窗口',
-      matchTime: 10000,
-      actionMaximum: 1,
-      resetMatch: 'app',
       rules: [
         {
+          key: 0,
           fastQuery: true,
+          matchTime: 10000,
+          actionMaximum: 1,
+          resetMatch: 'app',
           activityIds: '.MainTabActivity',
           matches:
             '[vid="floating_window"] >2 [vid="close"][clickable=true][visibleToUser=true]',
           snapshotUrls: 'https://i.gkd.li/i/20277426',
+        },
+        {
+          key: 1,
+          fastQuery: true,
+          actionMaximum: 1,
+          activityIds: '.feed.DetailWeiboActivity',
+          matches: '[vid="close_layout"][clickable=true][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/20277548',
         },
       ],
     },
