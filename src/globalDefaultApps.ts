155f8c94bafc63cf1e78bf7da801dc3286069b81
AIsouler <zy43220@gmail.com>
2025-06-08 14:35:38 +0800

    perf: rules

--- src/globalDefaultApps.ts
+++ src/globalDefaultApps.ts
@@ -146,6 +146,7 @@ export const blackListAppIDs: string[] = [
   'com.termux', // Termux
   'com.didjdk.adbhelper', // 甲壳虫ADB助手
   'com.qingmusic.changqing', // 青听音乐
+  'ru.tech.imageresizershrinker', // Image Toolbox
 
   // 游戏
   'com.ChillyRoom.DungeonShooter', // 元气骑士
