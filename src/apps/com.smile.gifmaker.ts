0cb4a982912126a6537ecdc96963f7dd69c83c5c
AIsouler <zy43220@gmail.com>
2025-06-12 12:44:33 +0800

    feat: rules

--- src/apps/com.smile.gifmaker.ts
+++ src/apps/com.smile.gifmaker.ts
@@ -6,7 +6,35 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '全屏广告-弹窗广告',
+      name: '全屏广告',
+      rules: [
+        {
+          key: 0,
+          fastQuery: true,
+          matchTime: 10000,
+          actionMaximum: 1,
+          resetMatch: 'app',
+          action: 'back',
+          activityIds: 'com.yxcorp.gifshow.HomeActivity',
+          matches: '[text="看视频领取" || text="立即领取"][visibleToUser=true]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/20083354',
+            'https://i.gkd.li/i/20083359',
+          ],
+        },
+        {
+          key: 1,
+          fastQuery: true,
+          activityIds: 'com.yxcorp.gifshow.HomeActivity',
+          matches:
+            '@ViewGroup[clickable=true] >2 [text="不感兴趣"][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/20700498',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '全屏广告-朋友推荐弹窗',
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
@@ -15,11 +43,8 @@ export default defineGkdApp({
           fastQuery: true,
           action: 'back',
           activityIds: 'com.yxcorp.gifshow.HomeActivity',
-          matches: '[text="看视频领取" || text="立即领取"][visibleToUser=true]',
-          snapshotUrls: [
-            'https://i.gkd.li/i/20083354',
-            'https://i.gkd.li/i/20083359',
-          ],
+          matches: '[text="朋友推荐"][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/20700504',
         },
       ],
     },
