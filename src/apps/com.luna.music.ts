00e2f30caec3ed41c97973f6622173ba649ee2d9
AIsouler <zy43220@gmail.com>
2026-01-09 16:51:06 +0800

    feat: rules

--- src/apps/com.luna.music.ts
+++ src/apps/com.luna.music.ts
@@ -7,7 +7,6 @@ export default defineGkdApp({
     {
       key: 1,
       name: '功能类-关闭广告的声音',
-      actionMaximum: 1,
       rules: [
         {
           fastQuery: true,
@@ -30,6 +29,61 @@ export default defineGkdApp({
           activityIds: 'com.luna.biz.main.main.MainActivity',
           matches: '[text="稍后再说"][visibleToUser=true]',
           snapshotUrls: 'https://i.gkd.li/i/21164341',
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '功能类-全自动看广告获取听歌时长',
+      rules: [
+        {
+          key: 0,
+          fastQuery: true,
+          actionDelay: 15000,
+          position: {
+            left: 'width * 7.45',
+            top: 'height * 0.5',
+          },
+          activityIds: 'com.ss.android.excitingvideo.ExcitingVideoActivity',
+          matches:
+            '@FlattenUIText[text="广告"] + [text$="声音"] + [text="反馈"][visibleToUser=true]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/24521423',
+            'https://i.gkd.li/i/24521440',
+          ],
+        },
+        {
+          key: 1,
+          activityIds:
+            'com.bytedance.sdk.openadsdk.core.component.reward.activity.TTRewardVideoActivity',
+          matches: '[visibleToUser=true][text="奖励已领取"]',
+          snapshotUrls: 'https://i.gkd.li/i/24522627',
+        },
+        {
+          preKeys: [0, 1],
+          fastQuery: true,
+          actionDelay: 1000,
+          activityIds: [
+            'com.bytedance.sdk.openadsdk.core.component.reward.activity.TTRewardVideoActivity',
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+            'com.ss.android.excitingvideo.ExcitingVideoActivity',
+          ],
+          matches:
+            '[text="领取奖励" || text^="再看一个" || text="继续观看"][visibleToUser=true]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/15140816',
+            'https://i.gkd.li/i/24521416',
+            'https://i.gkd.li/i/24521446',
+            'https://i.gkd.li/i/24521516',
+          ],
+        },
+        {
+          key: 99,
+          fastQuery: true,
+          activityIds: 'com.ss.android.excitingvideo.ExcitingVideoActivity',
+          matches:
+            '@[clickable=true][width<120 && height<120] + LinearLayout > [text="当前无新视频"][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/24522244',
         },
       ],
     },
