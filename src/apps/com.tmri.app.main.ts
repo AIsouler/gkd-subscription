65c954c7591113f6b45b3ba523344e783acab3ec
AIsouler <zy43220@gmail.com>
2026-02-10 18:44:48 +0800

    feat: rules

--- src/apps/com.tmri.app.main.ts
+++ src/apps/com.tmri.app.main.ts
@@ -5,6 +5,22 @@ export default defineGkdApp({
   name: '交管12123',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      priorityTime: 10000,
+      rules: [
+        {
+          fastQuery: true,
+          matches:
+            '[text*="跳过"][text.length<10][width<500 && height<300][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/i/25207240',
+        },
+      ],
+    },
+    {
       key: 1,
       name: '更新提示',
       fastQuery: true,
