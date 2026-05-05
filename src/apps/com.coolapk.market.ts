848f0a67052fc7ebd5e67706eb541d6334a2f376
AIsouler <zy43220@gmail.com>
2026-05-05 09:29:40 +0800

    feat: rules

--- src/apps/com.coolapk.market.ts
+++ src/apps/com.coolapk.market.ts
@@ -48,8 +48,11 @@ export default defineGkdApp({
         {
           key: 2,
           matches:
-            '@ImageView[childCount=0] < ViewGroup < ViewGroup +2 ViewGroup >2 [text="广告"][visibleToUser=true]',
-          snapshotUrls: 'https://i.gkd.li/i/27078867',
+            '@ImageView[childCount=0] < ViewGroup < ViewGroup +(2,3) ViewGroup >2 [text="广告"][visibleToUser=true]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/27078867',
+            'https://i.gkd.li/i/27380329',
+          ],
         },
       ],
     },
